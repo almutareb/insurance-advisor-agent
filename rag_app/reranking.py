466b7d1141fb3a5c996ace0fdffe44219d34de1c
Karan Singh <singh_karan@hotmail.com>
2024-07-12 12:49:30 +0530

    reranking with chroma fixed

--- rag_app/reranking.py
+++ rag_app/reranking.py
@@ -5,11 +5,13 @@ from dotenv import load_dotenv
 import os
 from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
 import requests
+from langchain_community.vectorstores import Chroma
+
 
 load_dotenv()
 
 
-def get_reranked_docs(query:str, 
+def get_reranked_docs_faiss(query:str, 
                       path_to_db:str, 
                       embedding_model:str,
                       hf_api_key:str, 
@@ -59,22 +61,72 @@ def get_reranked_docs(query:str,
         ranked_results = sorted(zip(docs, passages, relevance_scores), key=lambda x: x[2], reverse=True)
         top_k_results = ranked_results[:num_docs]
         return [doc for doc, _, _ in top_k_results]
+    
 
- 
+
+def get_reranked_docs_chroma(query:str, 
+                      path_to_db:str, 
+                      embedding_model:str,
+                      hf_api_key:str,
+                      reranking_hf_url:str = "https://api-inference.huggingface.co/models/sentence-transformers/all-mpnet-base-v2", 
+                      num_docs:int=5) -> list:
+    """ Re-ranks the similarity search results and returns top-k highest ranked docs
+
+        Args:
+            query (str): The search query
+            path_to_db (str): Path to the vectorstore database
+            embedding_model (str): Embedding model used in the vector store
+            num_docs (int): Number of documents to return
+        
+        Returns: A list of documents with the highest rank
+    """
+    assert num_docs <= 10, "num_docs should be less than similarity search results"
+    
+    embeddings = HuggingFaceInferenceAPIEmbeddings(api_key=hf_api_key,
+                                                   model_name=embedding_model)
+    # Load the vectorstore database
+    db = Chroma(persist_directory=path_to_db, embedding_function=embeddings)
+    
+    # Get 10 documents based on similarity search
+    sim_docs =  db.similarity_search(query=query, k=10)
+
+    # Add the page_content, description and title together
+    passages = [doc.page_content for doc in sim_docs]
+    
+    # Prepare the payload
+    payload = {"inputs": 
+               {"source_sentence": query,
+	            "sentences": passages}}
+
+    
+    headers = {"Authorization": f"Bearer {hf_api_key}"}
+
+    response = requests.post(url=reranking_hf_url, headers=headers, json=payload)
+    if response.status_code != 200:
+        print('Something went wrong with the response')
+        return
+    similarity_scores = response.json()
+    ranked_results = sorted(zip(sim_docs, passages, similarity_scores), key=lambda x: x[2], reverse=True)
+    top_k_results = ranked_results[:num_docs]
+    return [doc for doc, _, _ in top_k_results]
+    
+
+
 if __name__ == "__main__":
     
     HUGGINGFACEHUB_API_TOKEN = os.getenv('HUGGINGFACEHUB_API_TOKEN')
-    EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL")
-    
-    path_to_vector_db = Path("..")/'vectorstore/faiss-insurance-agent-500'
+    EMBEDDING_MODEL = "sentence-transformers/multi-qa-mpnet-base-dot-v1"
 
-    query = "Ich möchte wissen, ob ich meine geriatrische Haustier-Eidechse versichern kann"
-    
-    top_5_docs = get_reranked_docs(query=query,
-                                   path_to_db=path_to_vector_db,
-                                   embedding_model=EMBEDDING_MODEL,
-                                   hf_api_key=HUGGINGFACEHUB_API_TOKEN,
-                                   num_docs=5)
-    
-    for i, doc in enumerate(top_5_docs):
-        print(f"{i}: {doc}\n")
+    project_dir = Path().cwd().parent
+    path_to_vector_db = str(project_dir/'vectorstore/chroma-zurich-mpnet-1500')
+
+    query = "I'm looking for student insurance"
+
+
+    re_ranked_docs = get_reranked_docs_chroma(query=query,
+                                              path_to_db= path_to_vector_db,
+                                              embedding_model=EMBEDDING_MODEL,
+                                              hf_api_key=HUGGINGFACEHUB_API_TOKEN)
+
+
+    print(f"{re_ranked_docs=}")
