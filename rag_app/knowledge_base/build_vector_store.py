1e4bae2c3e6a7286eb82b44c62d3538470168860
isayhc <isayahculbertson@gmail.com>
2024-07-17 21:54:38 -0400

    replacing hardcoded values with constants

--- rag_app/knowledge_base/build_vector_store.py
+++ rag_app/knowledge_base/build_vector_store.py
@@ -9,9 +9,10 @@ from rag_app.utils.generate_summary import generate_description, generate_keywor
 import time
 import os
 
+from config import FAISS_INDEX_PATH
+
 def build_vector_store(
         docs: list, 
-        db_path: str, 
         embedding_model: str, 
         new_db:bool=False, 
         chunk_size:int=500, 
@@ -21,15 +22,16 @@ def build_vector_store(
 
     """
 
-    if db_path is None:
-        FAISS_INDEX_PATH = os.getenv("FAISS_INDEX_PATH")
-    else:
-        FAISS_INDEX_PATH = db_path
-
-    embeddings,chunks = create_embeddings(docs, chunk_size, chunk_overlap, embedding_model)
+    embeddings,chunks = create_embeddings(
+        docs, 
+        chunk_size, 
+        chunk_overlap, 
+        embedding_model
+        )
 
     #load chunks into vector store
     print(f'Loading chunks into faiss vector store ...')
+    
     st = time.time()
     if new_db:
         db_faiss = FAISS.from_documents(chunks, embeddings)
@@ -37,15 +39,18 @@ def build_vector_store(
     else:
         db_faiss = FAISS.add_documents(chunks, embeddings)
         bm25_retriever = BM25Retriever.add_documents(chunks)
+        
     db_faiss.save_local(FAISS_INDEX_PATH)
     et = time.time() - st
     print(f'Time taken: {et} seconds.')
 
     print(f'Loading chunks into chroma vector store ...')
+    
     st = time.time()
     persist_directory='./vectorstore/chroma-insurance-agent-1500'
     db_chroma = Chroma.from_documents(chunks, embeddings, persist_directory=persist_directory)
     et = time.time() - st
+    
     print(f'Time taken: {et} seconds.')
     result = f"built vectore store at {FAISS_INDEX_PATH}"
     return result
